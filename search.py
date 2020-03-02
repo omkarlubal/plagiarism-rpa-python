8ef46d98fb71874a1dc3bd9dcef2117e37628f16
Saurabh Daware <saurabhdaware99@gmail.com>
2020-03-02 12:29:26 +0530

    custom partial ratio function that returns matched substring indexes

--- search.py
+++ search.py
@@ -4,12 +4,12 @@
 
 from googleapiclient.discovery import build
 import json
-from fuzzywuzzy import fuzz
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from io import StringIO
+from difflib import SequenceMatcher
 
 import requests
 import sys
@@ -22,9 +22,9 @@ my_api_key = "AIzaSyBco2Cb9AikTpq74k70LyrJNBWDz9fQk9I"
 my_cse_id = "014265856113199739783:ik1fgtrafyo"
 
 
-# content = """
-# NBA operates on an online system of accreditation called "Accreditation Workflow Management System (AWMS), which includes institution registration.
-# """
+content = """
+NBA operates on an online system of accreditation called "Accreditation Workflow Management System (AWMS), which includes institution registration.
+"""
 
 def google_search(search_term, api_key, cse_id, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
@@ -37,16 +37,50 @@ def web_scrape(link):
     soup = BeautifulSoup(r.content, 'html.parser').text
     return soup
 
+def partial_ratio(s1, s2):
+  """"Return the ratio of the most similar substring
+  as a number between 0 and 100."""
+
+  if len(s1) <= len(s2):
+    shorter = s1
+    longer = s2
+  else:
+    shorter = s2
+    longer = s1
+
+  m = SequenceMatcher(None, shorter, longer)
+  blocks = m.get_matching_blocks()
+
+  scores = []
+  matched_blocks = []
+  for block in blocks:
+    long_start = block[1] - block[0] if (block[1] - block[0]) > 0 else 0
+    long_end = long_start + len(shorter)
+    long_substr = longer[long_start:long_end]
+
+    m2 = SequenceMatcher(None, shorter, long_substr)
+    matched_blocks.append(m2.get_matching_blocks())
+
+    r = m2.ratio()
+    scores.append(r)
+  
+  max_score = max(scores)
+  matching_block_of_max = matched_blocks[scores.index(max_score)]
+
+  substrings = []
+  for match in matching_block_of_max:
+      # print(shorter[match.a:match.a + match.size], end=" =?? ")
+      # print(long_substr[match.b:match.b + match.size])
+      substrings.append([match.b, match.b + match.size])
+
+  return [max_score*100, matching_block_of_max, substrings]
+
 
 def match_content(Str1, Str2):
-    partial_Ratio = fuzz.partial_ratio(Str1.lower(), Str2.lower())
-    # Ratio = fuzz.ratio(Str1.lower(),Str2.lower())
+    ratio, matched_blocks, substings = partial_ratio(Str1.lower(), Str2.lower())
 
-    return partial_Ratio
+    return ratio
 
-
-def check_plagiarism(scrapped, link):
-    return match_content(content, scrapped)
 
     # if(match_percentage > 30):
     #     print("Bro gotcha! {} percent copied from {}. What is this behaviour?".format(match_percentage, link))
@@ -84,13 +118,13 @@ def mainEngine(content):
     ans = ""
     for item in data['items']:
         if (item['link'].find('.pdf') > 0):
-            try:
-                urlretrieve(item['link'], "download.pdf")
-                scrapped_content = convert_pdf_to_txt("download.pdf");
-                match_content(content, scrapped_content)
-                # check_plagiarism(scrapped_content, item['link'])
-            except:
-                print("")
+            # try:
+            #     urlretrieve(item['link'], "download.pdf")
+            #     scrapped_content = convert_pdf_to_txt("download.pdf");
+            #     match_content(content, scrapped_content)
+
+            # except:
+            #     print("")
             continue
 
         scrapped_content = web_scrape(item['link'])
@@ -110,7 +144,7 @@ def mainEngine(content):
 
 
 # content = str(input())
-# print(mainEngine(content))
+print(mainEngine(content))
 
 # scrapped_content = web_scrape("https://en.wikipedia.org/wiki/Michael_Madana_Kama_Rajan")
 # match_content(content, scrapped_content)
